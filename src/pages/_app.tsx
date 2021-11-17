813042c968f62ec826eccba871b4a8d4da007504
voncardoso <vonharrison26@gmail.com>
2021-11-17 10:53:05 -0300

    delet switch

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app'
 import { GlobalStyle } from './../styles/global';
 
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
 
 
 function MyApp({ Component, pageProps }: AppProps) {
